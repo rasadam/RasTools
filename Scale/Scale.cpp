833816258f0c3db6d03bf1e4e43cca9bfb159f97
Adam Borenstein <adam@ras.land>
2021-09-26 16:37:21 +0300

    created basic scale class

---
+++ Scale/Scale.cpp
@@ -0,0 +1,13 @@
+#include "iObject.h"
+#include "Scale.h"
+
+using namespace std; 
+
+namespace RAS
+{
+
+Scale::Scale( string id ) : iObject(id) {
+
+}
+
+} // end of namespace
---
+++ Scale/Scale.h
@@ -0,0 +1,19 @@
+#include "iObject.h"
+
+#ifndef RAS_SCALE_H
+#define RAS_SCALE_H
+
+namespace RAS
+{
+
+class Scale :public iObject
+{
+
+Scale ( std::string  ID );
+
+
+}; //end of class
+
+} //end of namespace
+
+#endif //ifndef
