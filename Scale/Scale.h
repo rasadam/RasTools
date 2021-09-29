59781216f691fe47e8aa87e9a73e5fc51614dce0
Adam Borenstein <adam@ras.land>
2021-09-29 11:42:31 +0300

    added all data members

--- Scale/Scale.h
+++ Scale/Scale.h
@@ -8,10 +8,17 @@ namespace RAS
 
 class Scale :public iObject
 {
+public:
+    Scale ( std::string  ID );
 
-Scale ( std::string  ID );
-
-
+private:
+    // const string id (from base class)
+    const std::string type; 
+    const std::string subType; 
+    std::string cardName; 
+    std::string provideDate; 
+    std::string contractStart;
+    std::string contractEnd;
 }; //end of class
 
 } //end of namespace
