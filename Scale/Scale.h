a09c437222056ceba577db9dd64e1f0e139581e5
Adam Borenstein <adam@ras.land>
2021-09-30 13:13:46 +0300

    added inline getters and setters to data members

--- Scale/Scale.h
+++ Scale/Scale.h
@@ -10,6 +10,17 @@ class Scale :public iObject
 {
 public:
     Scale ( std::string  id, std::string cardName, std::string provideDate, std::string contractStart, std::string contractEnd );
+    inline bool operator== ( const Scale& other ){ return this->id == other.id && this->cardName == other.cardName; }
+
+    inline void SetCardName( std::string cardName ){ this->cardName = cardName; }
+    inline void SetProvideDate( std::string provideDate ){ this->provideDate = provideDate; } 
+    inline void SetContractStart( std::string contractStart ){ this->contractStart = contractStart; }
+    inline void SetContractEnd( std::string contractEnd){ this->contractEnd = contractEnd; }
+
+    inline std::string GetCardName() const { return cardName; }
+    inline std::string GetProvideDate() const { return provideDate; }
+    inline std::string GetContractStart() const { return contractStart; }
+    inline std::string GetContractEnd() const { return contractEnd; }
 
 private:
     // const string id (from base class)
