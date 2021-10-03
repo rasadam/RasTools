18e83599a95823b2b6705d28fdb2fb36008dcfad
Adam Borenstein <adam@ras.land>
2021-10-03 10:47:27 +0300

    compilation test for scale testing unit

---
+++ Testing/ScaleTest.cpp
@@ -0,0 +1,13 @@
+#include <Scale.h>
+#include <iostream> 
+
+using namespace RAS; 
+using namespace std; 
+
+int main(int argc, char const *argv[])
+{
+    cout << "tests 123" << endl;
+    return 0;
+}
+
+
