985ab50fb541f02757d76c2c84e0dd4e740e5f46
Adam Borenstein <adam@ras.land>
2021-10-03 12:04:47 +0300

    removed tests to seperate functions

--- Testing/ScaleTest.cpp
+++ Testing/ScaleTest.cpp
@@ -4,34 +4,37 @@
 using namespace RAS; 
 using namespace std; 
 
-int main(int argc, char const *argv[])
-{
-    iObject* scales[3];
+void InitScales( iObject* scales[] ){
     scales[0] = new Scale( "11853701", "עידן הקצבים מעלה אדומים", "מודל בסיס משקלי KH II 800", "27/01/21", "27/01/21", "27/01/22" );
     scales[2] = new Scale( "11853701", "עידן הקצבים מעלה אדומים", "מודל בסיס משקלי KH II 800", "27/01/21", "27/01/21", "27/01/22" );
     scales[1] = new Scale( "11789214", "הקצביה של אריה", "מודל בסיס משקלי KH II 800", "26/08/20", "26/08/20", "26/08/21" );
+}
 
-    for ( auto &&i : scales ) {
-        cout << i->GetID() << endl;
+void TestGetID( iObject* scales[], int size ){
+    for (size_t i = 0; i < size; ++i )
+    {
+        cout << scales[i]->GetID() << endl;
     }
+}
 
-    if ( scales[0]->operator==( *scales[1] ) ) {
+void CompareScales( iObject* scale1, iObject* scale2 ){
+    if ( scale1->operator==( *scale2 ) ) {
         cout << "0 && 1 are the same" << endl;
     } else {
         cout << "0 && 1 are NOT the same" << endl;
     }
-    
-    if ( scales[2]->operator==( *scales[1] ) ) {
-        cout << "2 && 1 are the same" << endl;
-    } else {
-        cout << "2 && 1 are NOT the same" << endl;
-    }
+}
 
-    if ( scales[0]->operator==( *scales[2] ) ) {
-        cout << "0 && 2 are the same" << endl;
-    } else {
-        cout << "0 && 2 are NOT the same" << endl;
-    }
+int main(int argc, char const *argv[])
+{
+    iObject* scales[3];
+
+    InitScales( scales );
+    TestGetID( scales, 3 );
+
+    CompareScales( scales[0], scales[1] );
+    CompareScales( scales[2], scales[1] );
+    CompareScales( scales[0], scales[2] );
     
     return 0;
 }
